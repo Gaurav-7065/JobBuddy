25599707602a79cc98123f425240ab57961ed9f0
Gaurav Jha <jhagaurav214@gmail.com>
2026-09-15 20:56:59 +0530

    changes in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,14 @@ import express from 'express'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
 import interviewRouter from './routes/interview.route.js'
+import 'dotenv/config'; 
 
 import cors from 'cors';
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: import.meta.env.VITE_CLIENT_URL,
+    origin: process.env.VITE_CLIENT_URL,
     credentials: true
 }));
 
